d45cc63aee20d9c44dbca959e995f2e28dc64d00
Javi Roberts <jrobertsotero@gmail.com>
2024-01-05 17:11:02 -0300

    static post rendering

---
+++ public/markdown/about.md
@@ -0,0 +1,11 @@
+# About me
+
+[//]: # (![Image]&#40;https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/DotOnline_gTLD_logo.svg/1024px-DotOnline_gTLD_logo.svg.png&#41;)
+
+I’m Javier, a Software Engineer from Argentina.
+
+I’m currently working as Wúru’s CTO.
+
+I love trail running 🏃, skiing ⛷️, sailing ⛵ and photography 📷.
+
+I invite you to read my blog, where (hopefully) I’ll write about tech & business stuff.
--- routes.go
+++ routes.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"backend/markdown"
 	"github.com/labstack/echo/v4"
+	"html/template"
 	"net/http"
+	"os"
 )
+
+type postBody struct {
+	Body template.HTML
+}
 
 func registerStaticRoutes(e *echo.Echo) {
 	e.Static("/static", "public/static")
@@ -13,4 +20,15 @@ func registerRoutes(e *echo.Echo) {
 	e.GET("/", func(c echo.Context) error {
 		return c.Render(http.StatusOK, "index", nil)
 	})
+	e.GET("/about", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "about", postBody{Body: loadMarkdownPost()})
+	})
+}
+
+func loadMarkdownPost() template.HTML {
+	content, err := os.ReadFile("public/markdown/about.md")
+	if err != nil {
+		panic(err)
+	}
+	return template.HTML(markdown.MDToHTML(content))
 }
